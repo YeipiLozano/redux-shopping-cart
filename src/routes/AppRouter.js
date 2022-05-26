237b988979c30b760ec2cfb330040c70a1d1d111
Pablo Lozano <juan.lozano@wizeline.com>
2022-05-26 13:13:50 -0500

    adding pages to router & navbar

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {Products} from '../pages/Products';
+import {Cart} from '../pages/Cart';
+import {Home} from '../pages/Home';
+import {Header} from '../components/Header';
+import { Login } from '../pages/Login';
 
 export const AppRouter = () => {
   return (
     <Router>
-      <div>
-        <header>
-          <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/products'>Products</Link>
-            CartLink
-          </nav>
-        </header>
-      </div>
+      <Header />
       <Switch>
         <Route exact path='/'>
-          Home
+          <Home />
         </Route>
-        <Route path='/products'>Products</Route>
-        <Route path='/cart'>Cart</Route>
+        <Route path='/products'>
+          <Products />
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
       </Switch>
     </Router>
   );
