35e625ec775f6665f0f85008f17dd0216c3f8825
Pablo Lozano <juan.lozano@wizeline.com>
2022-05-26 22:35:31 -0500

    ordered folder and styles structure

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Main} from '../styles/pages/Home.styles';
+import {HomeContainer} from './styles';
 
 export const Home = () => {
   return (
-    <Main>
+    <HomeContainer>
       <h1>Welcome to WizeStore!</h1>
       <p>
         Browse our <Link to='/products'>products</Link>
       </p>
-    </Main>
+    </HomeContainer>
   );
 };
---
+++ src/pages/home/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import {Container} from '../../styles/shared/Container';
+
+export const HomeContainer = styled(Container)`
+  height: 500px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
